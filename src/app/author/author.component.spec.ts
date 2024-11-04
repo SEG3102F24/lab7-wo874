a33657ad1a323dc3dda89cf17785dae2d507df19
wo874 <92176181+wo874@users.noreply.github.com>
2024-11-03 23:48:24 -0500

    Update author.component.spec.ts

--- src/app/author/author.component.spec.ts
+++ src/app/author/author.component.spec.ts
@@ -1,1 +1,30 @@
+import { Component } from '@angular/core';
+import { BooksService } from '../service/books.service';
+import { Author } from '../model/author.model'; 
+@Component({
+  selector: 'app-author',
+  templateUrl: './author.component.html',
+  styleUrls: ['./author.component.css']
+})
+export class AuthorComponent {
+  authorId: number | null = null;
+  author: Author | null = null;
+  message: string | null = null;
+
+  constructor(private booksService: BooksService) {}
+  fetchAuthor() {
+    if (this.authorId) {
+      this.booksService.getAuthor(this.authorId).subscribe({
+        next: (data: Author) => {
+          this.author = data;
+          this.message = null;
+        },
+        error: () => {
+          this.author = null;
+          this.message = 'Author not found';
+        }
+      });
+    }
+  }
+}
 
