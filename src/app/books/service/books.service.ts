e378da35f928e85bf73583d14fca5ceb0684d295
wo874 <92176181+wo874@users.noreply.github.com>
2024-11-03 23:51:28 -0500

    Update books.service.ts

--- src/app/books/service/books.service.ts
+++ src/app/books/service/books.service.ts
@@ -1,8 +1,8 @@
-import {inject, Injectable} from '@angular/core';
-import {Author, Book} from '../model/book';
+import { inject, Injectable } from '@angular/core';
+import { Author, Book } from '../model/book'; 
 import { HttpClient, HttpParams } from "@angular/common/http";
-import {Observable} from "rxjs";
-import {map} from 'rxjs/operators';
+import { Observable } from "rxjs";
+import { map } from 'rxjs/operators';
 
 const Url = 'http://localhost:8080/books-api/';
 
@@ -25,13 +25,18 @@ export class BooksService {
   }
 
   public getAuthorsNamed(firstName: string, lastName: string): Observable<any> {
-    const options = {params: new HttpParams().set('firstName', firstName).set('lastName', lastName)};
+    const options = { params: new HttpParams().set('firstName', firstName).set('lastName', lastName) };
     return this.http.get<any>(Url + 'authors', options).pipe(
-      map(response => response._embedded ? response._embedded.authors : undefined )
+      map(response => response._embedded ? response._embedded.authors : undefined)
     );
+  }
+
+  public getAuthor(id: number): Observable<Author> {
+    return this.http.get<Author>(`${Url}authors/${id}`);
   }
 
   public updateBookAuthors(bookId: number, authorId: number): Observable<any> {
     return this.http.patch(Url + 'books/' + bookId + '/authors/' + authorId, {});
   }
 }
+
